429eed016eeee08340e8f1d82469ca6f5947bbe6
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-04 23:29:50 -0500

    Corrected circumference formula

--- js/Circle.js
+++ js/Circle.js
@@ -3,8 +3,7 @@ class Circle {
     constructor(radius) {
         this.radius = radius;
         this.area = Math.PI * this.radius * this.radius;
-        // TODO: Look up formula for the circumference of a circle
-        this.circumference = Math.PI + this.area
+        this.circumference = 2 * Math.PI + this.radius;
     }
 
 }
